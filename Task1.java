e6cdc87113b89571e33842c7ce368ce48c43833f
MohammadMeharaj <147807340+MohammadMeharaj@users.noreply.github.com>
2023-11-07 13:52:05 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+class MyThread extends Thread{
+    public void run()
+    {
+        for(int i=1;i<5;i++)
+        {
+            System.out.println("Hi");
+        }
+    }   
+}
+public class Task1 {
+    public static void main(String[] args) {
+        MyThread t1=new MyThread();
+        t1.start();
+        for(int i=1;i<5;i++)
+        {
+            System.out.println("Hello");
+        }
+    }  
+}
